30cb13342aff7a807a1d670a5ecbc16811b942b0
xieyankun <kiroader@gmail.com>
2018-07-26 18:15:04 +0800

    inide

---
+++ src/main.js
@@ -0,0 +1,33 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue';
+// import App from './App';
+// import router from './router';
+
+Vue.config.productionTip = false;
+
+/* eslint-disable no-new */
+// new Vue({
+//   el: '#app',
+//   router,
+//   components: { App },
+//   template: '<App/>',
+// });
+
+// new Vue({
+//   el: '#app',
+//   router,
+//   render: h => h(App),
+// });
+
+new Vue({
+  el: '#app',
+  render: createElement => createElement('div', this.message, { message: 'hello world' }),
+  // render(createElement) {
+  //   return createElement('div', this.message, { message: 'hello world' });
+  // },
+  data: {
+    message: 'Hello world 000 ',
+  },
+});
+
